bc2ca6b559eb6f9ae4763e8bb8868c05463ffee9
SakRoi <roininensaku@gmail.com>
2023-10-16 13:21:17 +0300

    FEAT: Add hex validator to check the validity of given input, reformat the main function

--- HexToDecimal.c
+++ HexToDecimal.c
@@ -7,29 +7,124 @@ int hexLetterConverter(int c);
 int hexNumberConverter(int c);
 int lengthOfHexadecimal(char s[], int start);
 int hexValueCalculator(int pos, int hex);
-int convertHex(char s[], int start);
+int convertHex(char s[], int start, int end);
+int isHexValid(char s[], int start, int end);
 
 int main(int argc, char *argv[])
 {
+    int hexLength = 0, start = 0;
+
     if (argc < 2 && argc > 3){
         printf("Too many or too few arguments, give a hex number either with or without 0x");
         return 1;
     }
+
     char *hex = argv[1];
+
     if (hex[0] == '0' && (hex[1] == 'x' || hex[1] == 'X')){
-        printf("%d\n", convertHex(hex, 2));
-    } else {
-        printf("%d\n", convertHex(hex, 0));
+        start = 2;
+    }
+        
+    hexLength = lengthOfHexadecimal(hex, start);
+    if (!isHexValid(hex, start, hexLength)){
+        printf("Hex isn't valid\n");
+        return 1;
+    } 
+    else {
+        printf("%d\n", convertHex(hex, start, hexLength));
     }
 
     return 0;
 }
 
-int convertHex(char s[], int i)
+int isHexValid(char s[], int i, int hexLength)
+{
+    /*
+    Goes through all the valuable digits in the hex, 
+    returning a 0 (false) if it has a character not valid in the hexadecimal numerical system
+    and returning a 1 (true) if it has only valid characters
+    */
+    for (; hexLength != 0; ++i){
+        switch (s[i])
+        {
+        case 48: //ASCII value for 0, the following cases are 1-9
+        --hexLength;
+        break;
+        case 49:
+        --hexLength;
+        break;
+        case 50:
+        --hexLength;
+        break;
+        case 51:
+        --hexLength;
+        break;
+        case 52:
+        --hexLength;
+        break;
+        case 53:
+        --hexLength;
+        break;
+        case 54:
+        --hexLength;
+        break;
+        case 55:
+        --hexLength;
+        break;
+        case 56:
+        --hexLength;
+        break;
+        case 57: //9
+        --hexLength;
+        break;
+        case 65: //ASCII value for upper case a, the following cases are b-f
+        --hexLength;
+        break;
+        case 66:
+        --hexLength;
+        break;
+        case 67:
+        --hexLength;
+        break;
+        case 68:
+        --hexLength;
+        break;
+        case 69:
+        --hexLength;
+        break;
+        case 70: //upper case f
+        --hexLength;
+        break;
+        case 97: //ASCII value for lower case a, the following cases are b-f
+        --hexLength;
+        break;
+        case 98:
+        --hexLength;
+        break;
+        case 99:
+        --hexLength;
+        break;
+        case 100:
+        --hexLength;
+        break;
+        case 101:
+        --hexLength;
+        break;
+        case 102: //lower case f
+        --hexLength;
+        break;
+        default:
+        return 0; //the hex isn't valid
+        break;
+        }
+    }
+    return 1; //the hex is valid
+}
+
+int convertHex(char s[], int i, int hexLength)
 {
     int value = 0;
     char current = ' ';
-    int hexLength = lengthOfHexadecimal(s, i);
         for (; hexLength != 0 ; ++i){
             current = lower(s[i]);
             if (current >= 'a') {
